9e351c27e8d9125be608bb79d40b9692b2d9b875
NicolasIannello <nicolas.iannello99@gmail.com>
2022-05-01 06:43:42 -0300

    +brawl select legend

--- comms/brawlhalla.js
+++ comms/brawlhalla.js
@@ -1,10 +1,15 @@
 const needle = require('needle');
 const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
 
-async function image(){
+async function image(args){
+    if(args[0]=='' || args[0]==undefined){
+        var campeon='';
+    }else{
+        var campeon=args.shift();
+    }
 
     const params = {
-        'query': 'from:xdriphalla -is:retweet has:images',
+        'query': 'from:xdriphalla -is:retweet has:images '+campeon,
         'expansions': 'attachments.media_keys','media.fields': 'preview_image_url,url'
     }
 
@@ -21,15 +26,18 @@ async function image(){
         throw new Error('Unsuccessful request');
     }
 }
-async function tweet(message){
+async function tweet(message,args){
     try {
-        const response = await image();
+        const response = await image(args);
         /*console.dir(response, {
             depth: null
         });*/
-        var i=Math.floor(Math.random() * (response['meta'].result_count - 1 + 1) + 0);
-        message.channel.send(response['includes']['media'][i].url);
-
+        if(response['meta'].result_count==0){
+            message.channel.send('Por el momento no hay skins del campeon');
+        }else{
+            var i=Math.floor(Math.random() * (response['meta'].result_count - 1 + 1) + 0);
+            message.channel.send((i+1)+'/'+response['meta'].result_count+'\n'+response['includes']['media'][i].url);
+        }
     } catch (e) {
         console.log(e);
     }
