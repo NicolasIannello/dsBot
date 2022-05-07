e27c02a5281c189ebe3ed034f20fd9857554e819
NicolasIannello <nicolas.iannello99@gmail.com>
2022-05-07 09:38:41 -0300

    +masgrande

--- comms/play.js
+++ comms/play.js
@@ -3,6 +3,7 @@ const ytdl = require('ytdl-core');
 const { joinVoiceChannel,createAudioPlayer,createAudioResource,entersState,StreamType,AudioPlayerStatus,VoiceConnectionStatus, } = require('@discordjs/voice');
 var isPlaying=false;
 var queue=[],tiempo=[];
+const boca = ["https://imgur.com/MW4zdiA.png", "https://imgur.com/riL4WUY.png", "https://imgur.com/Qbvaevv.png", "https://imgur.com/cJRRTgA.png", "https://imgur.com/XiM7gGc.png", "https://imgur.com/iCs5PQU.png", "https://imgur.com/PBElkZ0.png", "https://imgur.com/3frkUvM.png", "https://imgur.com/nbAuGtd.png", "https://imgur.com/aG0BPWa.png"]
 
 async function reproducir(message,args,playlist){
     try{
@@ -29,7 +30,7 @@ async function reproducir(message,args,playlist){
             const rsrc = createAudioResource(stream,{inputType:StreamType.Arbitrary});
             queue.push(rsrc)
             var duracion = video.duration.toString().split(/ +/g);
-            tiempo.push((parseInt(duracion[0])+1)*1000)
+            tiempo.push((parseInt(duracion[0])+4)*1000)
             
             if(isPlaying==false){
                 play(connection,player);
@@ -57,7 +58,7 @@ async function reproducir(message,args,playlist){
 
             if (playlist==false){
                 message.channel.send(video.url+" ");
-                message.channel.send(video.duration.toString()+' '+tiempo[0]);
+                message.channel.send(video.duration.toString());
             }
         }
     }catch(error){
@@ -76,7 +77,7 @@ function play(connection,player){
         queue.shift();
         tiempo.shift();
         if(queue.length>0){
-            play();
+            play(connection,player);
         }else{
             try {
                 connection.destroy(); 
@@ -125,4 +126,37 @@ function skip(message){
     }
     
 }
-module.exports={reproducir,chao,skip};
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------
+async function masgrande(message,Membed){
+    var randomValue = boca[Math.floor(Math.random() * boca.length)];
+
+            const connection = await joinVoiceChannel({
+                channelId: message.member.voice.channel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator
+            })
+            
+            const player = createAudioPlayer();
+            const stream = ytdl("https://www.youtube.com/watch?v=Emp7ntPJm2w&ab_channel=chicho641",{filter:'audioonly',highWaterMark: 1 << 25,});
+            const rsrc = createAudioResource(stream,{inputType:StreamType.Arbitrary});
+                    
+            connection.subscribe(player);
+            player.play(rsrc);
+                        
+            setTimeout(() => {
+                try {
+                    connection.destroy();
+                } catch (error) {
+                    console.log(error);
+                }  
+            }, 150000);
+
+            Membed.setColor('#0099ff').setTimestamp().setFooter({text: 'CIVUS', iconURL: message.guild.iconURL()})
+            .setImage(randomValue).addFields({ name: 'Reproduciendo ahora: ', value: 'LA 12 PAPÁ', inline: false },);
+
+            message.channel.send({embeds: [Membed]});
+}
+
+module.exports={reproducir,chao,skip,masgrande};
